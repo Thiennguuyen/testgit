5db39f8504fde9ea7e0c5c8cf4db31a80671d6c3
Thiennguuyen <94065010+Thiennguuyen@users.noreply.github.com>
2023-05-18 17:13:11 +0700

    MVC: Router and Controller

--- src/index.js
+++ src/index.js
@@ -1,26 +1,37 @@
-const path = require('path')
-const express = require('express')
-const { engine } = require ('express-handlebars')
+const path = require("path");
+const express = require("express");
+const { engine } = require("express-handlebars");
 // const { handlebars } = require('express-handlebars')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+const route = require("./routes");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json());
 
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
-app.engine('hbs', engine({
-  extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.engine(
+    "hbs",
+    engine({
+        extname: ".hbs",
+    })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
